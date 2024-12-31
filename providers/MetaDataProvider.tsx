479e1f1889d3ed15f7abf04fcb4088b9355cfad8
Omraan Timmerarends <omraan93@gmail.com>
2024-12-31 16:51:47 +0100

    Fixed default selectedvehicle

--- providers/MetaDataProvider.tsx
+++ providers/MetaDataProvider.tsx
@@ -1,4 +1,7 @@
+import { GET_VEHICLES } from "@/graphql/queries";
+import { useVehicleStore } from "@/hooks/useVehicleStore";
 import { getOptimizedTrip } from "@/services/optimized-trips";
+import { useQuery } from "@apollo/client";
 import { useAuth, useOrganization, useUser } from "@clerk/clerk-expo";
 import polyline from "@mapbox/polyline";
 import { Position } from "@rnmapbox/maps/lib/typescript/src/types/Position";
@@ -11,27 +14,56 @@ const MetaDataContext = createContext<{
 	orgRole: string | undefined;
 	statusCategories: StatusCategory[] | null;
 } | null>(null);
-
+declare global {
+	interface UserUnsafeMetadata {
+		defaultMapView?: string;
+		organizations: {
+			[key: string]: {
+				vehicleId?: string;
+				labels?: string[];
+			};
+		};
+	}
+	interface UserPublicMetadata {
+		organizations: {
+			[key: string]: {
+				orgRole?: string;
+				vehicleId?: string;
+			};
+		};
+	}
+}
 export const MetaDataProvider = ({ children }: PropsWithChildren) => {
 	const [orgRole, setOrgRole] = useState<string | undefined>();
 
-	const { isSignedIn, getToken, orgId, userId, orgRole: authRole } = useAuth();
+	const { orgId, orgRole: authRole } = useAuth();
 	const { user } = useUser();
 
 	const { organization } = useOrganization();
 	const [statusCategories, setStatusCategories] = useState<StatusCategory[] | null>(null);
+
+	const { selectedVehicle, setSelectedVehicle } = useVehicleStore();
+	const { data: dataVehicles } = useQuery(GET_VEHICLES);
+	const vehicles = dataVehicles?.getVehicles || [];
+
 	useEffect(() => {
-		const metaDataLabels = user?.publicMetadata as UserPublicMetadata;
+		const metaData = user?.unsafeMetadata as UserPublicMetadata;
+		if (!orgId || !metaData || !metaData.organizations[orgId]) return;
+
+		const orgData = metaData.organizations[orgId];
 		if (authRole === "org:admin") {
 			setOrgRole("org:admin");
 		} else {
-			if (
-				orgId &&
-				metaDataLabels &&
-				metaDataLabels.organizations &&
-				metaDataLabels.organizations[orgId]?.orgRole
-			) {
-				setOrgRole(metaDataLabels.organizations[orgId].orgRole);
+			if (orgData.orgRole) {
+				setOrgRole(orgData.orgRole);
+			}
+		}
+		if (orgData.vehicleId && !selectedVehicle && vehicles && vehicles.length > 0) {
+			const newSelectedVehicle = vehicles.find(
+				(v: { name: string; value: Vehicle }) => v.name === orgData.vehicleId
+			);
+			if (newSelectedVehicle) {
+				setSelectedVehicle(newSelectedVehicle);
 			}
 		}
 		setStatusCategories(
